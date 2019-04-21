43e6f69096ab66055e9db1c0dffb154b1b2a2380
Ores7 <49046733+Ores7@users.noreply.github.com>
2019-04-20 21:29:57 -0500

    Iniciar-Registrar v02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 const config = {
   apiKey: "AIzaSyA9h7yXWuKwW07osly9aFGtqPtP7AIjYjY",
@@ -20,26 +21,14 @@ export default !firebase.apps.length ? firebase.initializeApp(config) : firebase
 
 export class AppComponent {
   title = 'TouristPlaces';
-  show:boolean = true;
-  Home:number = 1;
-  profile:number = 0;
-  ayuda:number = 0;
-  descuento:number = 0;
-  actualizacion:number=0;
-  turismo:number=0;
-  solicitud:number=0;
-  turismop1:number=0;
-  turismop2:number=0;
-  turismop3:number=0;
-
-  Adescuento:number=1;
-  Hdescuento:number=0;
 
   constructor() {
+
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         console.log('Sesion iniciada');
         // User is signed in.
+        CabeceraComponent.Dentro();
         var displayName = user.displayName;
         var email = user.email;
         var emailVerified = user.emailVerified;
@@ -50,10 +39,12 @@ export class AppComponent {
         console.log(email);
         // ...
       } else {
+        CabeceraComponent.fuera();
         console.log('Sesion no iniciada');
         // User is signed out.
         // ...
       }
     });
+
   }
 }
--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-cabecera',
@@ -7,8 +8,52 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class CabeceraComponent implements OnInit {
+  show:boolean = false;
+  home:number;
+  profile:number = 0;
+  ayuda:number = 0;
+  descuento:number = 0;
+  actualizacion:number=0;
+  turismo:number=0;
+  solicitud:number=0;
+  turismop1:number=0;
+  turismop2:number=0;
+  turismop3:number=0;
 
-  constructor() { }
+  Adescuento:number=1;
+  Hdescuento:number=0;
+  constructor() { 
+    this.home = 1;
+  }
+
+  cerr(){
+    firebase.auth().signOut()
+    .then(function(){
+      console.log("saliendo...")
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+  }
+
+  static Dentro(){
+
+    var contenido = document.getElementById("conten");
+    contenido.innerHTML = `
+    <div> USUARIO GENERICO  </div>
+    <div  (click)="this.cerr()"><button type="button"> CERRAR SESIÓN </button></div>
+    
+    `;
+  }
+
+  static fuera(){
+    var contenido = document.getElementById("conten");
+    contenido.innerHTML = `
+    <div><button class="butlogin"  type="button" data-toggle="modal" data-target="#myModal"> INICIAR SESIÓN </button></div>
+    <div class="butRegister"  type="button1"data-toggle="modal" data-target="#myModal2"> ¿No tienes usuario?   <br> Registrate  </div>
+    `;
+  }
+
 
   ngOnInit() {
   }
